3f7520edc4b73ae93ffadb6e0bbf8ddb1d220f31
Josué <vintetrezi@gmail.com>
2021-11-08 19:14:00 -0400

    First commit.

---
+++ estruturadados-algoritmos-java/src/com/josue/estruturajava/vetor/teste/Aula02.java
@@ -0,0 +1,18 @@
+package com.josue.estruturajava.vetor.teste;
+
+import java.util.Vector;
+
+import com.josue.estruturajava.vetor.Vetor;
+
+public class Aula02 {
+
+	public static void main(String[] args) {
+		
+		Vetor vetor = new Vetor(5);
+		
+		int i = 0;
+		i++;
+
+	}
+
+}
