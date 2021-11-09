e6f3a56f9a648306ad13867b981b789544c7126d
Josué <vintetrezi@gmail.com>
2021-11-08 20:13:20 -0400

    Arrays

---
+++ estruturadados-algoritmos-java/src/com/josue/estruturajava/vetor/teste/Aula03.java
@@ -0,0 +1,17 @@
+package com.josue.estruturajava.vetor.teste;
+
+import java.util.Vector;
+
+import com.josue.estruturajava.vetor.Vetor;
+
+public class Aula03 {
+
+	public static void main(String[] args) {
+		
+		Vetor vetor = new Vetor(2);
+	
+		vetor.adiciona("Elemento 1");
+		vetor.adiciona("Elemento 2");
+		vetor.adiciona("Elemento 3");	
+	}
+}
