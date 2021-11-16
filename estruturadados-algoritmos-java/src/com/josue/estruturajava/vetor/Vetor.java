d1af4e3822f85bf73426934db7f95242ec09e8c3
Josué <vintetrezi@gmail.com>
2021-11-16 06:52:07 -0300

    Add element in any position.

--- estruturadados-algoritmos-java/src/com/josue/estruturajava/vetor/Vetor.java
+++ estruturadados-algoritmos-java/src/com/josue/estruturajava/vetor/Vetor.java
@@ -46,6 +46,20 @@ public class Vetor {
 		return false;
 	}
 	
+	// ----------------------------------------------------------------------
+	public boolean adiciona(int posicao, String elemento) {
+		if(!(posicao >= 0 && posicao < tamanho)) {
+			throw new IllegalArgumentException("Argumento inválido. ");
+		}
+		for(int i = this.tamanho - 1; i >= posicao; i--) {
+			this.elementos[i+1] = this.elementos[i];
+		}
+		this.elementos[posicao] = elemento;
+		this.tamanho++;
+		return false;
+	}
+	//------------------------------------------------------------------------
+	
 	public String busca(int posicao) {
 		if(!(posicao >=0 && posicao < tamanho)) {
 			throw new IllegalArgumentException("\n\n\t >>> Posição inválida! <<<\n ");
