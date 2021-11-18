d4881b1c44692429191be20ad17a1c6a0786d267
Josué <vintetrezi@gmail.com>
2021-11-17 22:00:25 -0300

    Increases capacity.

--- estruturadados-algoritmos-java/src/com/josue/estruturajava/vetor/Vetor.java
+++ estruturadados-algoritmos-java/src/com/josue/estruturajava/vetor/Vetor.java
@@ -37,7 +37,7 @@ public class Vetor {
 	}*/
 	
 	public boolean adiciona(String elemento){
-		
+		this.aumentaCapacidade();
 		if(this.tamanho <= this.elementos.length) {
 			this.elementos[this.tamanho] = elemento;
 			this.tamanho++;
@@ -51,14 +51,27 @@ public class Vetor {
 		if(!(posicao >= 0 && posicao < tamanho)) {
 			throw new IllegalArgumentException("Argumento inválido. ");
 		}
-		for(int i = this.tamanho - 1; i >= posicao; i--) {
+		this.aumentaCapacidade();
+
+		 	for(int i = this.tamanho - 1; i >= posicao; i--) {
 			this.elementos[i+1] = this.elementos[i];
 		}
 		this.elementos[posicao] = elemento;
 		this.tamanho++;
-		return false;
+		return true;
 	}
 	//------------------------------------------------------------------------
+	
+	public void aumentaCapacidade() {
+		if(this.tamanho == this.elementos.length) {
+			String[] elementosNovos = new String[this.elementos.length * 2];
+			for(int i = 0; i < this.elementos.length; i++) {
+				elementosNovos[i] = this.elementos[i];
+			}
+			this.elementos = elementosNovos;
+		}
+	}
+	
 	
 	public String busca(int posicao) {
 		if(!(posicao >=0 && posicao < tamanho)) {
