beb429579444f2db2e7246751541002f95001b68
Josué <vintetrezi@gmail.com>
2021-11-24 06:37:17 -0300

    Batteries: Introduction.

---
+++ estruturadados-algoritmos-java/src/com/josue/estruturajava/pilha/Pilha.java
@@ -0,0 +1,14 @@
+package com.josue.estruturajava.pilha;
+
+import com.josue.estruturajava.base.EstruturaEstatica;
+
+public class Pilha<T> extends EstruturaEstatica<T>{
+
+	public Pilha() {
+		super();
+	}
+	public Pilha(int capacidade) {
+		super(capacidade);
+	}
+	
+}
---
+++ estruturadados-algoritmos-java/src/com/josue/estruturajava/pilha/teste/Aula13.java
@@ -0,0 +1,13 @@
+package com.josue.estruturajava.pilha.teste;
+
+import com.josue.estruturajava.pilha.Pilha;
+
+public class Aula13 {
+
+	public static void main(String[] args) {
+		Pilha<Integer> pilha = new Pilha<Integer>();
+		
+		System.out.println(pilha);
+		System.out.println(pilha.tamanho());
+	}
+}
